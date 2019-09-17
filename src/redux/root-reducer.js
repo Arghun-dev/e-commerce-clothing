fe8c3e356d245b247ae98faa0ee7d453dcc93ca5
Arghun <arghununiversity@gmail.com>
2019-09-17 17:29:07 +0430

    added redux-persist and modified our cardReducer with it and wrapped our app component with PersistGate

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage'
+
 import userReducer from './user/user.reducer';
 import cardReducer from './card/card.reducer';
 
-export default combineReducers({
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['card']
+}
+
+const rootReducer = combineReducers({
 	user: userReducer,
 	card: cardReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
+
